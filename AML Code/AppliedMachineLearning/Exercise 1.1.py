05f1f2cbe171d8b1f68f281732834250d0536e91
Thpri19 <98959938+Thpri19@users.noreply.github.com>
2023-09-19 10:18:46 +0200

    Add files via upload

---
+++ AML Code/AppliedMachineLearning/Exercise 1.1.py
@@ -0,0 +1,21 @@
+# Importing packages
+import numpy as np
+import tensorflow
+import pandas as pd
+import matplotlib
+import scipy
+import sklearn
+
+from sklearn.datasets import make_circles
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+from matplotlib import pyplot as plt
+
+'''
+Exercise 1.1
+
+
+'''
+
+
